99e1f6c6daa770c2ce719cd313a5052db7febce0
herservy <herservs@gmail.com>
2020-08-17 10:36:43 +0900

    update : solution - startGameTimer

--- js/main_solution.js
+++ js/main_solution.js
@@ -1,18 +1,91 @@
 'use strict';
 
-// 1. 게임 필드 획득
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 10;
+
 const field = document.querySelector('.game__field');
-// 2. 게임 필드의 전체 사이즈 정보 획득
 const fieldRect = field.getBoundingClientRect();
+
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+
+window.addEventListener('load', ()=> {   
+    console.log('load');
+});
+
+gameBtn.addEventListener('click', () => {
+
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+function showStopButton() {
+    const icon = document.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+    
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec);
+
+    timer = setInterval(() => {
+               
+        if(remainingTimeSec <= 0) {
+            clearInterval(timer);
+            return;
+        }
+
+        updateTimerText(--remainingTimeSec);        
+    }, 1000);
+}
+
+function updateTimerText(time) {
+    /* Math.floor : 인자 값에 소수점이 있으면 버림을 해주는 함수 */
+    const minutes = Math.floor(time / 60) ;
+    const seconds = Math.floor(time % 60) ;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function stopGame() {
+
+}
+
+
 
 
 // 3. 초기화 함수 : 벌레와 당근 생성 & 필드에 추가
 function initGame() {
-    console.log(fieldRect);
 
-    // 3-1. 벌레와 당근 생성 후 추가
-    addItem('carrot', 5, '../img/carrot.png');
-    addItem('bug', 5, '../img/bug.png');
+
+    field.innerHTML = '';
+    addItem('carrot', CARROT_COUNT, '../img/carrot.png');
+    addItem('bug', BUG_COUNT, '../img/bug.png');
 
 }
 
@@ -22,9 +95,22 @@ function addItem(className, count, imgPath) {
     // 4-1. 필드 요소의 좌표 기준 정하기 (game__field -> absolute, game -> relative)
     const x1 = 0;
     const y1 = 0;
-    const x2 = fieldRect.width - imgOffset(imgPath, 'x');
-    const y2 = fieldRect.height - imgOffset(imgPath, 'Y');
 
+    // const offsetX = imgOffset(imgPath, 'x');
+    // const offsetY = imgOffset(imgPath, 'y');
+    // const x2 = fieldRect.width - offsetX;
+    // const y2 = fieldRect.height - offsetY;
+    // console.log(`x = ${offsetX}, y = ${offsetY}`);
+
+    // const img = new Image();
+    // img.src = imgPath;
+    
+    // const x2 = fieldRect.width - img.width;
+    // const y2 = fieldRect.height - img.height;
+
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+    
 
 
     // 4-2. 벌레와 당근 생성 추가
@@ -50,35 +136,29 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function imgOffset(imgPath, axis) {
-    const img = new Image();
-    img.src = imgPath;
+/**
+ * DOM 트리에 등록되기까지 시간이 소요되는 작업
+ * 
+ * @param {*} imgPath 
+ * @param {*} axis 
+ * 
+ */
 
-    if (axis === '' || axis === null) {
-        alert('Function error : imgOffset() : Check parameter axis(1)');
-        return 0;
-    }
-    if (axis === 'x' || axis === 'X') {
-        console.log(`offset x : ${img.width}`);
-        return img.width;
-    } else if (axis === 'y' || axis === 'Y') {
-        console.log(`offset x : ${img.height}`);
-        return img.height;
-    } else {
-        alert('Function error : imgOffset() : Check parameter axis(2)');
-        return 0;
-    }
+// function imgOffset(imgPath, axis) {
+//     const img = new Image();
+//     img.src = imgPath;
 
-}
+//     if (axis === '' || axis === null) {
+//         alert('Function error : imgOffset() : Check parameter axis(1)');
+//         return 0;
+//     }
+//     if (axis === 'x' || axis === 'X') {
+//         return img.width;
+//     } else if (axis === 'y' || axis === 'Y') {
+//         return img.height;
+//     } else {
+//         alert('Function error : imgOffset() : Check parameter axis(2)');
+//         return 0;
+//     }
 
-function imgOffsetY(imgPath) {
-    const img = new Image();
-    const he = img.width;
-    return width;
-}
-
-
-
-
-
-initGame();
+// }
